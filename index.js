f7cae4a5bace16da943e31b8574f293212927cda
MahamudulHaque4 <hratul183@gmai.com>
2025-11-15 23:22:54 +0600

    My listing and My booking related Crud operation done

--- index.js
+++ index.js
@@ -31,6 +31,23 @@ async function run (){
 
         const db = client.db("rentwheel_DB");
         const carsCollection = db.collection('cars');
+        const listingsCollection = db.collection('listings');
+        const usersCollection = db.collection('users'); 
+
+        // Users API
+        app.post('/users', async (req, res) => {
+            const newuser = req.body;
+
+            const email = req.body.email;
+            const query = { email: email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.status(400).send({ message: 'User already exists' });
+            }
+            else
+                {const result = await usersCollection.insertOne(newuser);
+                 res.send(result);}
+        })
 
         // Find all cars
         app.get('/cars', async (req, res) => {
@@ -39,7 +56,21 @@ async function run (){
             //     carName: 1,
             //     image : 1,}
             // const cursor = carsCollection.find().sort({rentPrice: 1}).limit(5).project(projectFields);
-            const cursor = carsCollection.find();
+
+            console.log(req.query);
+            const email  = req.query.email;
+            const query = {}
+            if(email){
+                query.providerEmail  = email;
+            }
+
+            const cursor = carsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/top-cars', async (req, res) => {
+            const cursor = carsCollection.find().sort({rentPrice : -1}).limit(6);
             const result = await cursor.toArray();
             res.send(result);
         })
@@ -81,8 +112,41 @@ async function run (){
             };
             const result = await carsCollection.updateOne(query, updateDoc);
             res.send(result);
+        }) 
+
+        // Listings APIs can be added here similarly
+        app.get('/listings', async (req, res) => {
+            const email  = req.query.email;
+            const query = {}
+            if(email){
+                query.ownerEmail  = email;
+            }
+            const cursor = listingsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
         })
 
+        app.get('/listings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await listingsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/listings', async (req, res) => {
+            const listing = req.body;
+            const result = await listingsCollection.insertOne(listing);
+            res.send(result);
+        })
+
+        app.delete('/listings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await listingsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
       }
